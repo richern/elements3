8d17d76c688455275642b5d0c1cfa7f3902e9d78
Jauhar Basrai <jauhar.b@gmail.com>
2014-02-09 23:40:54 -0800

    fixing collision bugs

--- src/world/World.java
+++ src/world/World.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.geom.Line;
 import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Rectangle;
@@ -13,12 +14,8 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
 
 import physics.Player;
-import physics.Rectangle2;
 
 public class World {
-	
-	// physics
-	private static final float GRAVITY = 150;
 	
 	// fields
 	private Level level;
@@ -29,102 +26,59 @@ public class World {
 		player = new Player(level.getPlayerSpawn());
 	}
 	
-	public void update(GameContainer container, StateBasedGame sbg, int delta) {
-		updatePlayerPosition(delta);
+	public void update(Input input, int delta) {
+		updatePlayerPosition(input, delta);
 	}
 	 
-	public void render(GameContainer container, StateBasedGame sbg, Graphics graphics) {
+	public void render(Graphics graphics) {
 		level.getTileMap().render(0, 0);
-		graphics.draw(player.getShape().toRectangle());
+		graphics.draw(player.getRectangle());
 	}
 	
 	public Player getPlayer() {
 		return player;
 	}
 	
-	public void updatePlayerPosition(int delta) {
+	public void updatePlayerPosition(Input input, int delta) {
 		float time = delta / 1000.0f;
-		
-		float oldX = player.getPosition().getX();
-		float oldY = player.getPosition().getY();
-		
-		float velocityX = player.getVelocity().getX();
-		float velocityY = player.getVelocity().getY();
-		
-		// d1 = d0 + v*t
-		float newX = oldX + velocityX * time;
-		float newY = oldY - (velocityY - GRAVITY) * time;
-		
-		Point newPosition = checkCollision(new Point(newX, newY));
+		Point newPosition = checkCollision();
 		player.setPosition(newPosition);
 	}
 	
-	public Point checkCollision(Point position) {
-		Point newPosition = position;
-		int tileSize = level.getTileMap().getTileHeight();
-		boolean[][] blocked = level.getBlocked();
-		Rectangle2 rect = player.getShape();
+	public Point checkCollision() {
+		float newX = player.getPosition().getX();
+		float newY = player.getPosition().getY();
+		CollisionMap tileMap = level.getTileMap();
+		int tileSize = tileMap.getTileSize();
+		Rectangle rect = player.getRectangle();
 		
-		Line leftEdge = rect.getLeftEdge();
-		Line rightEdge = rect.getRightEdge();
-		Line bottomEdge = rect.getBottomEdge();
+		float minX = rect.getMinX();
+		float maxX = rect.getMaxX();
+		float minY = rect.getMinY();
+		float maxY = rect.getMaxY();
 		
-		checkLeftCollision(leftEdge, blocked, tileSize, newPosition);
-		checkBottomCollision(bottomEdge, blocked, tileSize, newPosition);
-		checkRightCollision(rightEdge, blocked, tileSize, newPosition);
+		Point topLeft = new Point(minX, minY);
+		Point topRight = new Point(maxX, minY);
+		Point bottomLeft = new Point(minX, maxY);
+		Point bottomRight = new Point(maxX, maxY);
 		
-		return newPosition;
-	}
-	
-	public void checkLeftCollision(Line bottomEdge, boolean[][] blocked, int tileSize, Point newPosition) {
-		float pixelX = bottomEdge.getCenterX();
-		float pixelY1 = bottomEdge.getMinY();
-		float pixelY2 = bottomEdge.getMaxY();
+		boolean isTopLeftBlocked = tileMap.isBlocked(topLeft);
+		boolean isTopRightBlocked = tileMap.isBlocked(topRight);
+		boolean isBottomLeftBlocked = tileMap.isBlocked(bottomLeft);
+		boolean isBottomRightBlocked = tileMap.isBlocked(bottomRight);
 		
-		int tileX = (int) (pixelX / tileSize);
-		int tileY1 = (int) (pixelY1 / tileSize);
-		int tileY2 = (int) (pixelY2 / tileSize);
+		boolean isTopBlocked = isTopLeftBlocked || isTopRightBlocked;
+		boolean isRightBlocked = isTopRightBlocked || isBottomRightBlocked;
+		boolean isBottomBlocked = isBottomLeftBlocked || isBottomRightBlocked;
+		boolean isLeftBlocked = isTopLeftBlocked || isBottomLeftBlocked;
 		
-		for(int tileY = tileY1; tileY <= tileY2; tileY++) {
-			if(blocked[tileX][tileY]) {
-				float tileRight = tileX * tileSize + tileSize + player.getRadius();
-				newPosition.setX(tileRight);
-			}
+		if(isBottomBlocked) {
+			newY = ((int) maxY / tileSize) * tileSize - player.getHeight() / 2 - 1;
 		}
-	}
-	
-	public void checkRightCollision(Line rightEdge, boolean[][] blocked, int tileSize, Point newPosition) {
-		float pixelX = rightEdge.getCenterX();
-		float pixelY1 = rightEdge.getMinY();
-		float pixelY2 = rightEdge.getMaxY();
-		
-		int tileX = (int) (pixelX / tileSize);
-		int tileY1 = (int) (pixelY1 / tileSize);
-		int tileY2 = (int) (pixelY2 / tileSize);
-		
-		for(int tileY = tileY1; tileY <= tileY2; tileY++) {
-			if(blocked[tileX][tileY]) {
-				float tileRight = tileX * tileSize - player.getRadius() - 1 ;
-				newPosition.setX(tileRight);
-			}
+		if(isTopLeftBlocked) {
+			newX = ((int) minX / tileSize) * tileSize + tileSize + player.getWidth() / 2;
 		}
-	}
-	
-	public void checkBottomCollision(Line bottomEdge, boolean[][] blocked, int tileSize, Point newPosition) {
-		float pixelX1 = bottomEdge.getMinX();
-		float pixelX2 = bottomEdge.getMaxX();
-		float pixelY = bottomEdge.getCenterY();
 		
-		int tileX1 = (int) (pixelX1 / tileSize);
-		int tileX2 = (int) (pixelX2 / tileSize);
-		int tileY = (int) (pixelY / tileSize);
-		
-		for(int tileX = tileX1; tileX <= tileX2; tileX++) {
-			if(blocked[tileX][tileY]) {
-				float tileTop = tileY * tileSize - player.getRadius();
-				newPosition.setY(tileTop);
-			}
-		}
+		return new Point(newX, newY);
 	}
-	
 }
