fbdc7eab20923214bc331769405f184666cd016e
Jauhar Basrai <jauhar.b@gmail.com>
2014-03-15 20:52:16 -0700

    imported new tileset

---
+++ resources/tilesets/ground96.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground96" tilewidth="96" tileheight="96">
+ <image source="../../../../Pictures/Elements 3/ground96.png" width="96" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="blocked" value="true"/>
+  </properties>
+ </tile>
+</tileset>
